719a5a03975234fad1128c6bf70b4b031d809cec
felipe <felipefaraggi@gmail.com>
2018-07-08 19:00:39 +0200

    most of the grunt work is done. only missing checking for pair and separating volume and price in file parsing

--- exchanges/management/commands/get_prices.py
+++ exchanges/management/commands/get_prices.py
@@ -1,7 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
-from polls.models import Question as Poll
 from arboto.settings import DEBUG
+import os
 
+from exchanges.models import Ask, Bid, ApiRequest, Exchange
+
+from django.utils.dateparse import parse_datetime
 from django.core.management.base import BaseCommand
 
 # not useful anymore because we don't have to execute outside files anymore.
@@ -11,12 +14,83 @@ from django.core.management.base import BaseCommand
 #     # TODO
 #     ENV_PATH = ''
 
+PRICE_TYPES = (
+    ('a', 'ask'),
+    ('b', 'bid')
+    )
+
+EXCHANGES = [
+	'kraken',
+	'bitstamp',
+	'buda',
+	'cryptomkt'
+    ]
 
 class Command(BaseCommand):
     help = 'getting prices from pyarboto data files'
 
     def handle(self, *args, **options):
-        for data_file in os.listdir('../../../pyarboto/data'):
-            if not data_file.endswith('placeholder'):
-                with open(os.path.join('../../../pyarboto/data', data_file)) as f:
-                    content = f.read()
+        for data_file in os.listdir('/home/felipe/code/chainimpact/pyarboto/pyarboto/data'):
+            if data_file.endswith('placeholder'):
+                break
+            with open(os.path.join('/home/felipe/code/chainimpact/pyarboto/pyarboto/data', data_file)) as f:
+                price_type = self.get_price_type(data_file)
+                exchange = self.get_exchange(data_file)
+                pair = self.get_pair(data_file)
+
+                data = f.readlines()
+                if len(data) > 1:
+                    penultimate_line = data[-2]
+
+                    # check if already imported
+                    # if self.check_date_previous_import(penultimate_line):
+
+                last_line = data[-1]
+                print(last_line, exchange, price_type)
+                self.import_data(last_line, exchange, price_type)
+
+    def get_exchange(self, data_file):
+        exchanges = EXCHANGES
+        for possible_exchange in exchanges:
+            if data_file.lower().startswith(possible_exchange.lower()):
+                exchange = possible_exchange
+        return Exchange.objects.get(name=exchange)
+
+    def get_price_type(self, data_file):
+        if data_file.endswith('ASKS'):
+            return 'a'
+        return 'b'
+
+    def get_pair(self, data_file):
+        # pass
+
+    def check_date_previous_import(self, penultimate_line):
+        date_sting = penultimate_line[:16]
+        parse_datetime(date_sting)
+        if Ask.objects.filter(timestamp=date):
+            # check if
+            return True
+        else:
+            # TODO: re-import data from previous date
+            pass
+
+    def import_data(self, last_line, exchange, price_type):
+        date_sting = last_line[:16]
+        date = parse_datetime(date_sting)
+
+        data_points = last_line.split()
+        for point in data_points[2:]:
+            if price_type == 'a':
+                new_data_point = Ask(
+                    timestamp = date,
+                    value = point,
+                    exchange = exchange
+                )
+                print(new_data_point)
+            else:
+                new_data_point = Bid(
+                    timestamp = date,
+                    value = point,
+                    exchange = exchange
+                )
+                print(new_data_point)
