4a7079f52411f6eb2177ea43f72ef5b1b2e64c4e
felipe <felipefaraggi@gmail.com>
2018-07-08 11:03:38 +0200

    added bid and ask (and price abstract) models

--- exchanges/models.py
+++ exchanges/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+PRICE_TYPES = (
+    ('a', 'ask'),
+    ('b', 'bid')
+    )
 
 class Exchange(models.Model):
     name = models.CharField(max_length=200)
@@ -9,3 +13,26 @@ class Exchange(models.Model):
 
     def __str__(self):
         return "{}".format(self.name)
+
+
+class Price(models.Model):
+    timestamp = models.DateTimeField('date published')
+    value = models.DecimalField(max_digits=16, decimal_places=10)
+    exchange = models.ForeignKey(Exchange, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class Ask(Price):
+    price_type = models.CharField(choices=PRICE_TYPES, default='a', max_length=1)
+
+    def __str__(self):
+        return "{}-{}-{}-{}".format(self.exchange, self.timestamp, self.price_type, self.value)
+
+
+class Bid(Price):
+    price_type = models.CharField(choices=PRICE_TYPES, default='a', max_length=1)
+
+    def __str__(self):
+        return "{}-{}-{}".format(self.exchange, self.timestamp, self.value)
