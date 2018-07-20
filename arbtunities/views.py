86fbb48d2306775e34c6a4b270212749d450c17b
v-almonacid <ealmonacid@gmail.com>
2018-07-20 16:00:20 +0200

    modified API view

--- arbtunities/views.py
+++ arbtunities/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from exchanges.models import Ask, Bid
 from datetime import datetime
+from django.core import serializers
 
 # just some testing
 def index(request):			
@@ -11,14 +12,16 @@ def monitor(request):
 	return render(request, 'arbtunities/monitor.html')
 
 def monitor_data(request):
-	pair = request.GET.get('pair', 'ETHEUR')	
-	return JsonResponse(get_data_by_pair(pair))
+	pair = request.GET.get('pair', 'ETHEUR')
+	exchange = request.GET.get('exch', 'kraken')
+	count = request.GET.get('count', 100) # to-do: replace by time fraime (1h, 4h, 1d, etc)	
+	return JsonResponse(get_weighted_price(exchange, pair, count), safe=False)
 
-def get_data_by_pair(pair='ETHEUR', count=100):
-	# TO-DO: for all exchanges
-	kraken = get_weighted_price('kraken', 'ETHEUR', count)
-	cryptomkt = get_weighted_price('cryptomkt', 'ETHEUR', count)
-	return {'kraken': kraken, 'cryptomkt': cryptomkt}
+# def get_data_by_pair(pair='ETHEUR', count=100):
+# 	# TO-DO: for all exchanges
+# 	kraken = get_weighted_price('kraken', 'ETHEUR', count)
+# 	cryptomkt = get_weighted_price('cryptomkt', 'ETHEUR', count)
+# 	return {'kraken': kraken, 'cryptomkt': cryptomkt}
 	
 def get_weighted_price(exchange='kraken', pair='ETHEUR', count=100):	
 	# get all distinct timestamps, starting from the newest
@@ -41,4 +44,5 @@ def get_weighted_price(exchange='kraken', pair='ETHEUR', count=100):
 			p_vs_t.append({'timestamp': t[0], 'price_avg': price_avg})		
 	# array is reversed to be ordered in time
 	p_vs_t.reverse()
+	print(p_vs_t)
 	return p_vs_t
