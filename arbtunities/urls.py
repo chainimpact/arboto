8103065ee175f9b77d05a5a24896882db54745dc
v-almonacid <ealmonacid@gmail.com>
2018-08-13 12:40:56 +0200

    added a navbar

--- arbtunities/urls.py
+++ arbtunities/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),    
-    path('monitor/', views.monitor, name='monitor'),
+    path('monitor/price_dist', views.monitor, name='monitor'),
     path('monitor/rois', views.rois, name='rois'),
-    path('monitor2/', views.monitor2, name='monitor2'),
+    path('monitor/history', views.monitor2, name='monitor2'),
     path('api/', views.monitor_data, name='monitor_data'),
     path('api/rois', views.rois_data, name='rois_data'),
     path('api/monitor2', views.monitor2_data, name='monitor2_data'),
